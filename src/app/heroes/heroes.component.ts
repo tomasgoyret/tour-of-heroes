3274d29c9ae8e1b3f2a9a251c20ff269ba9f3241
tomasgoyret <tomas.goyret@gmail.com>
2022-05-24 16:18:58 -0300

    Add additional messages to hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,6 +15,7 @@ export class HeroesComponent implements OnInit {
 
   constructor(
     private heroService: HeroService,
+    private messageService: MessageService,
   ) { }
 
   ngOnInit(): void {
@@ -22,6 +24,7 @@ export class HeroesComponent implements OnInit {
 
   onSelected(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
