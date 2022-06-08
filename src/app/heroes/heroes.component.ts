bae12925e66310b652d2ef6865d6a302cc9a56bd
tomasgoyret <tomas.goyret@gmail.com>
2022-06-08 18:29:18 -0300

    update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,7 +24,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(h => {
-        console.log(h, "suscribe")
         this.heroes = h
       });
   }
